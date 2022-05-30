48fd4f4016d3c17cf6a39b3e5888e740c8ee45f5
Denis Smyslov <dsmyslov@protonmail.com>
2022-05-30 04:25:39 +0300

    added wzn file upload

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
 st.header("Store Performance Analysis")
+
+st.subheader("Upload files")
+
+wzn_file=None
+wzn_file=st.file_uploader("Upload Walzon report here",type=["xlsx","csv"])
+st.caption("""The following fields in the excel or csv file are expected:\
+	"Order item id","Amz order id","Asin","Order date","Latest ship date",\
+	"Orderfulfillment status","Market item cost","Quantity ordered"
+											""")
