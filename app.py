3917b27dc96a75cd85e1b7c4078f749cadf73d94
Denis Smyslov <dsmyslov@protonmail.com>
2022-05-30 05:47:55 +0300

    added a lot of functions

--- app.py
+++ app.py
@@ -13,9 +13,6 @@ st.caption("""The following fields in the excel or csv file are expected:\
 	"Orderfulfillment status","Market item cost","Quantity ordered"
 											""")
 
-azn_files=None
-azn_files=st.file_uploader("Upload Amazon report here",type=["xlsx","csv"],
-	accept_multiple_files=True)
 
 
 if wzn_file:
@@ -49,7 +46,18 @@ if wzn_file:
 
 	wzn.sort_values(by="Order date",ignore_index=True,inplace=True)
 	wzn.columns=wzn.columns.str.replace(" ","_").str.lower()
-	#st.dataframe(wzn)
+
+	col_name='order_date'
+	col_idx=wzn.columns.tolist().index(col_name)
+	first_order_date=wzn.iloc[-1,col_idx]
+	last_order_date=wzn.iloc[0,col_idx]
+	col1,col2 = st.columns(2)
+	col1.markdown(f"**walzon first_order_date:** {first_order_date}")
+	col2.write(f"**walzon last_order_date:** {last_order_date}")
+
+	azn_files=None
+	azn_files=st.file_uploader("Upload Amazon report here",type=["xlsx","csv"],
+													accept_multiple_files=True)
 
 
 	st.subheader("Walzon shipped orders")
@@ -57,9 +65,13 @@ if wzn_file:
 	wzn_shipped.set_index("order_date",inplace=True)
 
 
+
+
 	st.dataframe(wzn_shipped)
 	st.markdown("**Chart 1. Payments for orders shipped**")
 	st.bar_chart(data=wzn_shipped[['order_cost']])
+
+
 
 	capital_invested=st.text_input("Enter invested capital amount",value='15000')
 	capital_invested=int(capital_invested)
@@ -72,6 +84,40 @@ if wzn_file:
 	st.line_chart(data=wzn_shipped[['cash_balance']])
 	st.caption("""This chart includes only amounts paid for order purchases\n
 		Amounts received from Amazon and Funds returned will be included in the next release""")
+
+	# load and pre-process amazon reports
+
+	azn=pd.DataFrame()
+	for fn in azn_files:
+	  azn=pd.concat([azn,pd.read_csv(fn)])
+
+	azn.drop_duplicates(subset=['Order ID'],inplace=True)
+	azn.Date=pd.to_datetime(azn.Date).dt.tz_localize(tz='US/Central')
+	azn.sort_values(by='Date',inplace=True,ascending=False,ignore_index=True)
+	azn.columns=azn.columns.str.replace(" ","_").str.lower()
+	azn['date']=azn.date.dt.strftime("%Y-%m-%d")
+	azn.rename(columns={"order_id":'amz_order_id'},inplace=True)
+
+	st.dataframe(azn)
+
+	st.subheader("Profit from Operations")
+
+	df=azn.merge(wzn,on="amz_order_id",how="left")
+	df.rename(columns={"Total (USD)":"net_amz_payment",
+	                   "Transaction type":"transaction_type",
+	                   "Market item cost":"market_item_cost"},inplace=True)
+
+	
+	df_result=df.query("transaction_type=='Order Payment'")
+	df_result['profit']=df_result['total_(usd)']-df_result['order_cost']
+	st.markdown(f"**Profit from transactions:** {df_result['profit'].sum()}")
+	st.markdown("Table 4. Walzon file merge with Amazon reports")
+	st.dataframe(df_result)
+	st.caption("""
+		Table 4 includes those transaction only which were included by Amazon into uploaded reports\
+		 and for which Order Payments were made. <NA> means that transactions included into Amazon\
+		 report were not included into Walzon report. 
+		""")
 
 	# except:
 	# 	st.write("failed to upload; please check that the file is excel or csv and if all required columns are in the file ")
